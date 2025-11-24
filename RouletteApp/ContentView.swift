0b26cbeca924cacf90284074ece6762a5c1fbd90
youfuji <youfuji1116@gmail.com>
2025-11-24 16:08:35 +0900

    Initial Commit

---
+++ RouletteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouletteApp
+//
+//  Created by 藤原洋希 on 2025/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouletteApp/RouletteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteAppApp.swift
+//  RouletteApp
+//
+//  Created by 藤原洋希 on 2025/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RouletteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
